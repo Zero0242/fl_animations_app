ff9c45f409314c3bbc9a71623008c35cbb139a6e
Zero0242 <pedrocorream66@gmail.com>
2023-07-08 02:48:55 -0400

    header design

--- lib/pages/emergency_page.dart
+++ lib/pages/emergency_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class EmergengyPage extends StatelessWidget {
   const EmergengyPage({super.key});
@@ -13,6 +14,52 @@ class EmergengyPage extends StatelessWidget {
 
 class IconHeader extends StatelessWidget {
   const IconHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        const _IconBackground(),
+        Positioned(
+          top: -50,
+          left: -70,
+          child: FaIcon(
+            FontAwesomeIcons.plus,
+            size: 250,
+            color: Colors.white.withOpacity(0.2),
+          ),
+        ),
+        Column(
+          children: [
+            const SizedBox(height: 80, width: double.infinity),
+            Text(
+              'Has solicitado',
+              style:
+                  TextStyle(fontSize: 20, color: Colors.white.withOpacity(0.7)),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Asistencia Medica',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: Colors.white.withOpacity(0.7),
+              ),
+            ),
+            const FaIcon(
+              FontAwesomeIcons.plus,
+              size: 80,
+              color: Colors.white,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _IconBackground extends StatelessWidget {
+  const _IconBackground();
 
   @override
   Widget build(BuildContext context) {
