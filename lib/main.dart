950776a67395c0eb773ca2bdf047bfc0d813d9ee
Zero0242 <pedrocorream66@gmail.com>
2024-10-01 19:27:37 -0300

    [SETUP]: app inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'config/config.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,12 +11,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Animaciones",
+      theme: AppTheme(isDarkMode: false).getTheme(),
+      // routes: AppRouter.rutasApp,
+      // initialRoute: AppRouter.root,
+      home: const Scaffold(),
     );
   }
 }
