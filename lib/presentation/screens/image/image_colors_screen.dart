53f36ff6594a0f54298821ff6fad070c4e8f0902
Zero0242 <pedrocorream66@gmail.com>
2024-11-30 18:48:12 -0300

    [IMAGE]: dominant colors

--- lib/presentation/screens/image/image_colors_screen.dart
+++ lib/presentation/screens/image/image_colors_screen.dart
@@ -3,6 +3,7 @@ import 'dart:typed_data';
 import 'package:fl_animaciones/config/config.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 class ImageColorsScreen extends StatefulWidget {
   const ImageColorsScreen({super.key});
@@ -17,7 +18,7 @@ class _ImageColorsScreenState extends State<ImageColorsScreen> {
 
   List<Color> get imageColors {
     if (_pickedFile == null) return [];
-    return DominantColors(bytes: _pickedFile!, dominantColorsCount: 3)
+    return DominantColors(bytes: _pickedFile!, dominantColorsCount: 8)
         .extractDominantColors();
   }
 
@@ -26,6 +27,15 @@ class _ImageColorsScreenState extends State<ImageColorsScreen> {
     if (result == null) return;
     _pickedFile = await result.readAsBytes();
     setState(() {});
+  }
+
+  Future<List<Color>> loadColors() async {
+    if (_pickedFile == null) return [];
+    final res = await PaletteGenerator.fromImageProvider(
+      MemoryImage(_pickedFile!),
+      maximumColorCount: 8,
+    );
+    return res.colors.toList();
   }
 
   @override
@@ -58,6 +68,29 @@ class _ImageColorsScreenState extends State<ImageColorsScreen> {
                     color: color,
                   ),
               ],
+            ),
+            const SizedBox(height: 30),
+            FutureBuilder<List<Color>>(
+              future: loadColors(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (snapshot.hasData) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      for (final color in snapshot.requireData)
+                        Container(
+                          height: 70,
+                          width: 30,
+                          color: color,
+                        ),
+                    ],
+                  );
+                }
+                return Text('Error al cargar ${snapshot.error}');
+              },
             ),
           ],
         ),
